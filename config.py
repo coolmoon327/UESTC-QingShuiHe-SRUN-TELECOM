8e43cf44ed52be72032f91e60527306fa8c5e2e6
coolmoon327 <35034472+coolmoon327@users.noreply.github.com>
2022-01-09 20:57:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 debug = True  # 在控制台显示log
-username = ""  # 您的学号
-passowrd = ""  # 您的密码
+username = ""  # 您的电信宽带账号（默认为手机号）
+passowrd = ""  # 您的电信宽带密码（默认 12345678）
 check_time = 2  #时间间隔
 log = True  # 是否记录log
 log_path = "./"  # log记录路径，默认为当前路径
